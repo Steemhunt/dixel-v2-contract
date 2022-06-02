547a32c76d72660fb23e5f8fb6a9b2c07a06f1c2
yoonsung <ys.choi@me.com>
2022-06-02 17:35:58 +0900

    Missed Loop op

--- contracts/lib/StringUtils.sol
+++ contracts/lib/StringUtils.sol
@@ -7,9 +7,13 @@ pragma solidity ^0.8.13;
 library StringUtils {
     function contains(string memory haystack, bytes1 needle) internal pure returns (bool) {
         bytes memory haystackBytes = bytes(haystack);
-        for (uint256 i = 0; i < haystackBytes.length; i++) {
+        uint256 length = haystackBytes.length;
+        for (uint256 i; i != length; ) {
             if (haystackBytes[i] == needle) {
                 return true;
+            }
+            unchecked {
+                ++i;
             }
         }
 
