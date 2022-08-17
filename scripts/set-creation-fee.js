8e286fb0a5a1bd154d6677de52b31cabd6835f61
Sebastian Kim <sydneyitguy@gmail.com>
2022-08-17 13:43:42 +0900

    Update creation fee on OKC, Polygon

--- scripts/set-creation-fee.js
+++ scripts/set-creation-fee.js
@@ -10,13 +10,22 @@ async function main() {
     bscmain: {
       factory: '0x690A9370f69058541fb0945185C8af308E799ba9',
       fee: ether('0.5')
-    }
+    },
+    okcmain: {
+      factory: '0x82b91E6DEDE8B8acDADe2212983DF946CA695d1e',
+      fee: ether('5')
+    },
+    polygonmain: {
+      factory: '0xa8F498E42884677b4055bEE3cc9970f4A8555ff9',
+      fee: ether('100')
+    },
   };
 
   const DixelClubV2Factory = await hre.ethers.getContractFactory('DixelClubV2Factory');
   const factory = await DixelClubV2Factory.attach(config[hre.network.name].factory);
 
-  // await factory.updateCreationFee(config[hre.network.name].fee.toString());
+  const tx = await factory.updateCreationFee(config[hre.network.name].fee.toString());
+  await tx.wait(2);
 
   const updatedFee = await factory.creationFee();
   console.log(`Creation fee updated on ${hre.network.name} = ${updatedFee / 1e18}`);
@@ -31,3 +40,5 @@ main()
 
 // npx hardhat run --network klaytnmain scripts/set-creation-fee.js
 // npx hardhat run --network bscmain scripts/set-creation-fee.js
+// npx hardhat run --network okcmain scripts/set-creation-fee.js
+// npx hardhat run --network polygonmain scripts/set-creation-fee.js
