e5a8dd569a889994b12c3da7abfea0bc9ee70aec
Sebastian Kim <sydneyitguy@gmail.com>
2023-02-27 18:10:32 +0900

    Put original code back

--- contracts/DixelClubV2NFT.sol
+++ contracts/DixelClubV2NFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; // FIXME: remove
 import "base64-sol/base64.sol";
 import "./lib/ERC721Initializable.sol";
 import "./lib/ERC721Queryable.sol";
