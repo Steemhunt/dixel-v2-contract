c1a49ccef291ee79c80dd57976563a5ceb139e4e
Sebastian Kim <sydneyitguy@gmail.com>
2025-02-11 15:26:30 +0900

    Update creation/flat fees

--- scripts/set-creation-fee.js
+++ scripts/set-creation-fee.js
@@ -3,21 +3,25 @@ const { ether } = require("@openzeppelin/test-helpers");
 
 async function main() {
   const config = {
-    // ethmain: {
-    //   factory: '0x66BF6409A52E634262BD04c0005562f229b03778',
-    //   fee: ether('0.000111') // 0.000111 ETH (~$0.3)
-    // },
+    ethmain: {
+      factory: '0x43c71D3fAb7CE185C4D569f8fc3C93dF95334c57',
+      fee: ether('0.000111') // 0.000111 ETH (~$0.3)
+    },
+    base: {
+      factory: '0x8b0Fdc91A4e0259ec6a29b3507a7Ea6E1d04A8fF',
+      fee: ether('0.000111') // 0.000111 ETH (~$0.3)
+    },
     // same for all ETH based chains
     klaytnmain: {
-      factory: '0x31B8eb1d3DcB2C333e5d70cAA022855ffdBD0fDA',
+      factory: '0x82b91E6DEDE8B8acDADe2212983DF946CA695d1e',
       fee: ether('3') // 3 KLAY (~$0.3)
     },
     bscmain: {
-      factory: '0xB76110E9cb56c0dba1596F3413A6DB9023e36463',
+      factory: '0x9326c3A25935d11a6510a0A4eb18233AB2963f38',
       fee: ether('0.0005') // 0.0005 BNB (~$0.3)
     },
     polygonmain: {
-      factory: '0xa8F498E42884677b4055bEE3cc9970f4A8555ff9',
+      factory: '0x0E5F42f6308C13dBf9c7e18B5bd956048A50486f',
       fee: ether('1') // 1 POL (~$0.3)
     },
   };
@@ -33,8 +37,8 @@ async function main() {
   const tx2 = await factory.updateFlatFee(config[hre.network.name].fee.toString());
   await tx2.wait(2);
 
-  const updatedFee = await factory.creationFee();
-  console.log(`Creation fee and flat fee updated on ${hre.network.name} = ${updatedFee / 1e18}`);
+  console.log(`Creation fee updated on ${hre.network.name} = ${await factory.creationFee() / 1e18}`);
+  console.log(`Flat fee updated on ${hre.network.name} = ${await factory.flatFee() / 1e18}`);
 }
 
 main()
